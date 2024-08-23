04da0d895930b12f27a1928505764bf9c0c3fe61
Kudo Chien <kudo@expo.dev>
2024-08-24 02:01:29 +0800

    decorate forwardRef types using generic

--- packages/expo/build/dom/webview-wrapper.d.ts
+++ packages/expo/build/dom/webview-wrapper.d.ts
@@ -1,5 +1,5 @@
 import React from 'react';
 import type { DOMPropsWithSource } from './dom.types';
-declare const RawWebView: React.ForwardRefExoticComponent<Omit<DOMPropsWithSource, "ref"> & React.RefAttributes<unknown>>;
+declare const RawWebView: React.ForwardRefExoticComponent<Omit<DOMPropsWithSource, "ref"> & React.RefAttributes<object>>;
 export default RawWebView;
 //# sourceMappingURL=webview-wrapper.d.ts.map
--- packages/expo/src/dom/webview-wrapper.tsx
+++ packages/expo/src/dom/webview-wrapper.tsx
@@ -34,137 +34,140 @@ function mergeRefs(...props) {
   };
 }
 
-const RawWebView = React.forwardRef(({ dom, source, ...marshalProps }: DOMPropsWithSource, ref) => {
-  const webviewRef = React.useRef<WebView>(null);
-  const [containerStyle, setContainerStyle] = React.useState<WebViewProps['containerStyle']>(null);
+const RawWebView = React.forwardRef<object, DOMPropsWithSource>(
+  ({ dom, source, ...marshalProps }, ref) => {
+    const webviewRef = React.useRef<WebView>(null);
+    const [containerStyle, setContainerStyle] =
+      React.useState<WebViewProps['containerStyle']>(null);
 
-  const setRef = React.useMemo(() => mergeRefs(webviewRef, {}, ref), [webviewRef, ref]);
+    const setRef = React.useMemo(() => mergeRefs(webviewRef, {}, ref), [webviewRef, ref]);
 
-  const emit = React.useCallback(
-    (detail: BridgeMessage<any>) => {
-      webviewRef.current?.injectJavaScript(getInjectEventScript(detail));
-    },
-    [webviewRef]
-  );
+    const emit = React.useCallback(
+      (detail: BridgeMessage<any>) => {
+        webviewRef.current?.injectJavaScript(getInjectEventScript(detail));
+      },
+      [webviewRef]
+    );
 
-  // serializable props, action names.
+    // serializable props, action names.
 
-  const smartActions = Object.entries(marshalProps).reduce<{
-    props: Record<string, any>;
-    names: string[];
-  }>(
-    (acc, [key, value]) => {
-      if (value instanceof Function) {
-        acc.names.push(key);
-      } else {
-        // TODO: Recurse and assert that nested functions cannot be used.
-        acc.props[key] = value;
-      }
-      return acc;
-    },
-    { names: [], props: {} }
-  );
-
-  // When the `marshalProps` change, emit them to the webview.
-  React.useEffect(() => {
-    emit({ type: '$$props', data: smartActions });
-  }, [emit, smartActions]);
-
-  return (
-    <WebView
-      webviewDebuggingEnabled={__DEV__}
-      originWhitelist={['*']}
-      allowFileAccess
-      allowFileAccessFromFileURLs
-      allowsAirPlayForMediaPlayback
-      allowsFullscreenVideo
-      containerStyle={containerStyle}
-      {...dom}
-      injectedJavaScriptBeforeContentLoaded={[
-        getInjectEnvsScript(),
-        // On first mount, inject `$$EXPO_INITIAL_PROPS` with the initial props.
-        `window.$$EXPO_INITIAL_PROPS = ${JSON.stringify(smartActions)};true;`,
-        dom?.autoSize ? getInjectBodySizeObserverScript() : null,
-        dom?.injectedJavaScriptBeforeContentLoaded,
-        'true;',
-      ]
-        .filter(Boolean)
-        .join('\n')}
-      ref={setRef}
-      source={source}
-      style={[
-        dom?.style
-          ? { flex: 1, backgroundColor: 'transparent' }
-          : { backgroundColor: 'transparent' },
-        dom?.style,
-      ]}
-      onMessage={(event) => {
-        const { type, data } = JSON.parse(event.nativeEvent.data);
-
-        if (type === AUTOSIZE_EVENT) {
-          if (dom?.autoSize) {
-            setContainerStyle({
-              width: data.width,
-              height: data.height,
-            });
-          }
-          return;
-        }
-
-        if (type === NATIVE_ACTION) {
-          const action = getNativeAction(marshalProps, data.actionId);
-          if (action == null) {
-            throw new Error(`Native action "${data.actionId}" is not defined.`);
-          }
-          if (typeof action !== 'function' || !(action instanceof Function)) {
-            throw new Error(`Native action "${data.actionId}" is not a function.`);
-          }
-
-          const emitError = (error) => {
-            emit({
-              type: NATIVE_ACTION_RESULT,
-              data: {
-                uid: data.uid,
-                actionId: data.actionId,
-                error: serializeError(error),
-              },
-            });
-          };
-          const emitResolve = (result?: any) => {
-            // Send async results back to the DOM proxy for return values.
-            emit({
-              type: NATIVE_ACTION_RESULT,
-              data: {
-                uid: data.uid,
-                actionId: data.actionId,
-                result,
-              },
-            });
-          };
-          try {
-            const value = action(...data.args);
-            if (value instanceof Promise) {
-              return value
-                .then((result) => {
-                  emitResolve(result);
-                })
-                .catch((error) => {
-                  emitError(error);
-                });
-            } else {
-              // Send async results back to the webview proxy for return values.
-              return emitResolve(value);
-            }
-          } catch (error) {
-            return emitError(error);
-          }
+    const smartActions = Object.entries(marshalProps).reduce<{
+      props: Record<string, any>;
+      names: string[];
+    }>(
+      (acc, [key, value]) => {
+        if (value instanceof Function) {
+          acc.names.push(key);
         } else {
-          dom?.onMessage?.(event);
+          // TODO: Recurse and assert that nested functions cannot be used.
+          acc.props[key] = value;
         }
-      }}
-    />
-  );
-});
+        return acc;
+      },
+      { names: [], props: {} }
+    );
+
+    // When the `marshalProps` change, emit them to the webview.
+    React.useEffect(() => {
+      emit({ type: '$$props', data: smartActions });
+    }, [emit, smartActions]);
+
+    return (
+      <WebView
+        webviewDebuggingEnabled={__DEV__}
+        originWhitelist={['*']}
+        allowFileAccess
+        allowFileAccessFromFileURLs
+        allowsAirPlayForMediaPlayback
+        allowsFullscreenVideo
+        containerStyle={containerStyle}
+        {...dom}
+        injectedJavaScriptBeforeContentLoaded={[
+          getInjectEnvsScript(),
+          // On first mount, inject `$$EXPO_INITIAL_PROPS` with the initial props.
+          `window.$$EXPO_INITIAL_PROPS = ${JSON.stringify(smartActions)};true;`,
+          dom?.autoSize ? getInjectBodySizeObserverScript() : null,
+          dom?.injectedJavaScriptBeforeContentLoaded,
+          'true;',
+        ]
+          .filter(Boolean)
+          .join('\n')}
+        ref={setRef}
+        source={source}
+        style={[
+          dom?.style
+            ? { flex: 1, backgroundColor: 'transparent' }
+            : { backgroundColor: 'transparent' },
+          dom?.style,
+        ]}
+        onMessage={(event) => {
+          const { type, data } = JSON.parse(event.nativeEvent.data);
+
+          if (type === AUTOSIZE_EVENT) {
+            if (dom?.autoSize) {
+              setContainerStyle({
+                width: data.width,
+                height: data.height,
+              });
+            }
+            return;
+          }
+
+          if (type === NATIVE_ACTION) {
+            const action = getNativeAction(marshalProps, data.actionId);
+            if (action == null) {
+              throw new Error(`Native action "${data.actionId}" is not defined.`);
+            }
+            if (typeof action !== 'function' || !(action instanceof Function)) {
+              throw new Error(`Native action "${data.actionId}" is not a function.`);
+            }
+
+            const emitError = (error) => {
+              emit({
+                type: NATIVE_ACTION_RESULT,
+                data: {
+                  uid: data.uid,
+                  actionId: data.actionId,
+                  error: serializeError(error),
+                },
+              });
+            };
+            const emitResolve = (result?: any) => {
+              // Send async results back to the DOM proxy for return values.
+              emit({
+                type: NATIVE_ACTION_RESULT,
+                data: {
+                  uid: data.uid,
+                  actionId: data.actionId,
+                  result,
+                },
+              });
+            };
+            try {
+              const value = action(...data.args);
+              if (value instanceof Promise) {
+                return value
+                  .then((result) => {
+                    emitResolve(result);
+                  })
+                  .catch((error) => {
+                    emitError(error);
+                  });
+              } else {
+                // Send async results back to the webview proxy for return values.
+                return emitResolve(value);
+              }
+            } catch (error) {
+              return emitError(error);
+            }
+          } else {
+            dom?.onMessage?.(event);
+          }
+        }}
+      />
+    );
+  }
+);
 
 function serializeError(error: any) {
   if (error instanceof Error) {
